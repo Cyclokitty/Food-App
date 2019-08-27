0e4424f9a1a5727ecdb93a70f9cf7ea1986b2f25
Cyclokitty <laura@vingada.com>
2019-08-27 16:25:53 -0400

    started README page, added useEffect to call yelp data in SearchScreen, set up hooks folder

---
+++ src/screens/README.md
@@ -0,0 +1,4 @@
+# Practicing React Hooks 
+# Better data & state management
+# Doing the exercises before the video segment
+# Making notes for Future Projects
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,17 +6,32 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async() => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'Toronto'
-            }
-        });
-        setResults(response.data.businesses);
+    const searchApi = async(searchTerm) => {
+        console.log('hi here');
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'Toronto'
+                }
+            });
+            setResults(response.data.businesses);
+        }
+        catch(error) {
+            console.log(`Error: ${error.message}`);
+            setErrorMessage('Sorry, error has occurred. Please try later');
+            
+        }
     };
+
+    // call searchApi() when component is first rendered
+    //searchApi('pasta');
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
 
     console.log(results);
 
@@ -25,9 +40,10 @@ const SearchScreen = () => {
             <SearchBar 
                 term={term}
                 onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => searchApi()}
+                onTermSubmit={() => searchApi(term)}
             />
             <Text>We have found {results.length} results so far.</Text>
+            {errorMessage.length > 0 ? <Text>Sorry, network error. Please try again later</Text> : null}
         </View>
     )
 };
